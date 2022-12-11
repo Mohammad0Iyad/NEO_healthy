40db6bd64778645e575e47ac874683b7804dc800
mohammad <mohammad0iyad@gmail.com>
2022-12-11 14:12:32 +0200

    basic code

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Neo   Demo',
+      title: 'Neo Demo',
       theme: ThemeData(
 
         primarySwatch: Colors.amber,
