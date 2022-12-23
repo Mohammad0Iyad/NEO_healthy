8b00b21b5d69e27b1887de2e041d5ef9ef4a1ca1
FlutterDeveloper96 <alfaqihh1998@gmail.com>
2022-12-24 01:30:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return SplashPage();
+    return HomePage();
   }
 }
