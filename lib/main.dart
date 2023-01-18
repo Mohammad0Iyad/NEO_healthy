0a73727fa88800a7dcec010a37f7e301e5d69d68
Mohammad Iyad <mohammad0iyad@gmail.com>
2023-01-18 20:14:57 +0300

    test for me

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       create: (BuildContext context) { FirebaseAuthMethods(FirebaseAuth.instance); },
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Neoo Demo',
+        title: 'Neooo Demo',
         theme: ThemeData(
           scaffoldBackgroundColor: Colors.white,
           primarySwatch: Colors.green,
